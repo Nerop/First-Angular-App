665dd4df7b330969be1dfa8e5e12ebd3cee5c52d
Vladyslav Sazonov <nerop30@gmail.com>
2016-11-01 10:53:18 +0200

    customized personal pages

--- src/js/controllers/adminController.js
+++ src/js/controllers/adminController.js
@@ -2,18 +2,23 @@ scotchApp.controller('adminController', function ($scope) {
 
     $('select').material_select();
     $('.change-user').change(function () {
-        if ($( ".change-user :selected" ).val() === '1'){
+        if ($(".change-user :selected").val() === '1') {
             location.href = "#me";
-        }
-        else if ($( ".change-user :selected" ).val() === '2'){
+        } else if ($(".change-user :selected").val() === '2') {
             location.href = "#admin";
-        }
-        else if ($( ".change-user :selected" ).val() === '3'){
+        } else if ($(".change-user :selected").val() === '3') {
             location.href = "#contentmanager";
-        }
-        else {
+        } else {
             location.href = "#hotelsowner";
         }
+    });
+
+    $('#btn-update-delete').leanModal({
+        dismissible: false
+    });
+    $('#btn-update-delete').click(function () {
+        console.log('jasd');
+        $('#modal2').modal('open');
     });
 
     $scope.message = 'hello, its me - mario';
